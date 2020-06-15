9500c522e5c352c8ef2bfbf4b918d7aeafddd3ad
Chris Banes <chrisbanes@google.com>
2020-06-15 12:16:47 +0000

    Try out Compose/UI Snapshot 6589751

--- buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
@@ -51,7 +51,7 @@ object Libs {
         }
 
         object UI {
-            const val snapshot = "6581424"
+            const val snapshot = "6589751"
             const val version = "0.1.0-SNAPSHOT"
 
             const val core = "androidx.ui:ui-core:$version"
