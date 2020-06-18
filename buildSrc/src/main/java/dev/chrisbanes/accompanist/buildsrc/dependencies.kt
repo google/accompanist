dcaac9668298de9fe838221cae043399f3541ceb
Chris Banes <chrisbanes@google.com>
2020-06-18 12:07:44 +0100

    Update to AGP 4.2.0-alpha01

--- buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:4.1.0-alpha10"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:4.2.0-alpha01"
 
     const val gradleMavenPublishPlugin = "com.vanniktech:gradle-maven-publish-plugin:0.11.1"
 
