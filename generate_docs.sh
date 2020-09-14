5df1d07ecf67b194b7ce425882e4c3ad6726fcda
Chris Banes <chrisbanes@google.com>
2020-09-14 17:39:59 +0100

    Tidy up links

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Work around Dokka failing to link against external links generated from 'gfm' sources.
+wget -O package-list-coil-base https://coil-kt.github.io/coil/api/coil-base/package-list
+sed -i '' 's/$dokka.linkExtension:md/$dokka.linkExtension:html/g' package-list-coil-base
+
+# Build the coil-base docs.
+./gradlew clean dokka
+
+rm package-list-coil-base
+
 cp README.md docs/index.md
 cp CONTRIBUTING.md docs/contributing.md
 cp images/social.png docs/header.png
