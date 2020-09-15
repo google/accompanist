149bdcd343b74ce1562bf8b21377152d70282b95
Chris Banes <chrisbanes@google.com>
2020-09-15 08:47:51 +0100

    Add Actions to deploy docs

--- generate_docs.sh
+++ generate_docs.sh
@@ -2,7 +2,7 @@
 
 # Work around Dokka failing to link against external links generated from 'gfm' sources.
 wget -O package-list-coil-base https://coil-kt.github.io/coil/api/coil-base/package-list
-sed -i '' 's/$dokka.linkExtension:md/$dokka.linkExtension:html/g' package-list-coil-base
+sed -i.bak 's/$dokka.linkExtension:md/$dokka.linkExtension:html/g' package-list-coil-base
 
 # Build the coil-base docs.
 ./gradlew clean dokka
@@ -13,15 +13,15 @@ cp README.md docs/index.md
 cp CONTRIBUTING.md docs/contributing.md
 cp images/social.png docs/header.png
 
-sed -i '' 's/CONTRIBUTING.md/contributing/' docs/index.md
-sed -i '' 's/coil\/README.md/coil/' docs/index.md
-sed -i '' 's/images\/social.png/header.png/' docs/index.md
+sed -i.bak 's/CONTRIBUTING.md/contributing/' docs/index.md
+sed -i.bak 's/coil\/README.md/coil/' docs/index.md
+sed -i.bak 's/images\/social.png/header.png/' docs/index.md
 
 cp coil/README.md docs/coil.md
 mkdir -p docs/coil
 cp coil/images/crossfade.gif docs/coil/crossfade.gif
 
-sed -i '' 's/images\/crossfade.gif/crossfade.gif/' docs/coil.md
+sed -i.bak 's/images\/crossfade.gif/crossfade.gif/' docs/coil.md
 
 # Convert docs/xxx.md links to just xxx/
-sed -i '' 's/docs\/\([a-zA-Z-]*\).md/\1/' docs/index.md
+sed -i.bak 's/docs\/\([a-zA-Z-]*\).md/\1/' docs/index.md
