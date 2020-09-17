643d4872c401041387e715e14070b8519512fd05
Chris <chris@banes.me>
2020-09-17 15:37:03 +0100

    Update to Compose SNAPSHOT 6838769

--- buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
@@ -57,7 +57,7 @@ object Libs {
         }
 
         object Compose {
-            const val snapshot = "6834837"
+            const val snapshot = "6838769"
             const val version = "1.0.0-SNAPSHOT"
 
             const val runtime = "androidx.compose.runtime:runtime:$version"
