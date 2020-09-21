6d790d30810f089c23f5ff8a47644fe79b0f0e69
Chris <chris@banes.me>
2020-09-21 15:09:15 +0100

    Tidy up Dokka GFM output

--- generate_docs.sh
+++ generate_docs.sh
@@ -27,3 +27,14 @@ sed -i.bak 's/images\/crossfade.gif/crossfade.gif/' docs/coil.md
 
 # Convert docs/xxx.md links to just xxx/
 sed -i.bak 's/docs\/\([a-zA-Z-]*\).md/\1/' docs/index.md
+
+#########################
+# Tidy up Dokka output
+#########################
+
+# Remove all of the line breaks in the docs
+find docs/api/ -name '*.md' -exec sed -i.bak 's/<br><br>//g' {} \;
+# Remove the random androidJvm headers
+find docs/api/ -name '*.md' -exec sed -i.bak 's/\[*androidJvm\]*//g' {} \;
+# Remove the 'Brief description' headers
+find docs/api/ -name '*.md' -exec sed -i.bak 's/Brief description//g' {} \;
