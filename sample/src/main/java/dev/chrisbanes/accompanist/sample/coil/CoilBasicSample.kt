68485ccf186c66cd8235036aa810fc05ae084189
Chris Banes <chris@banes.me>
2020-10-14 10:43:39 +0100

    Add sample using aspect ratio + crop

--- sample/src/main/java/dev/chrisbanes/accompanist/sample/coil/CoilBasicSample.kt
+++ sample/src/main/java/dev/chrisbanes/accompanist/sample/coil/CoilBasicSample.kt
@@ -25,9 +25,11 @@ import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayout
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.layout.preferredWidth
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -35,6 +37,7 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ContextAmbient
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
@@ -154,6 +157,14 @@ private fun Sample() {
                             CircularProgressIndicator(Modifier.align(Alignment.Center))
                         }
                     }
+                )
+
+                // CoilImage with an aspect ratio and crop scale
+                CoilImage(
+                    data = randomSampleImageUrl(),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.preferredWidth(256.dp)
+                        .aspectRatio(16 / 9f)
                 )
             }
         }
