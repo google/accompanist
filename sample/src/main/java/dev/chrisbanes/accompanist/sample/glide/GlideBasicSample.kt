21e57575f23427f808f536dc1e1a915a1f7f0342
Chris Banes <chris@banes.me>
2020-10-14 14:35:31 +0100

    Update to main

--- sample/src/main/java/dev/chrisbanes/accompanist/sample/glide/GlideBasicSample.kt
+++ sample/src/main/java/dev/chrisbanes/accompanist/sample/glide/GlideBasicSample.kt
@@ -23,9 +23,11 @@ import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayout
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.layout.preferredWidth
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -33,6 +35,7 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -111,6 +114,14 @@ private fun Sample() {
                             CircularProgressIndicator(Modifier.align(Alignment.Center))
                         }
                     }
+                )
+
+                // GlideImage with an aspect ratio
+                GlideImage(
+                    data = randomSampleImageUrl(),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.preferredWidth(256.dp)
+                        .aspectRatio(16 / 9f)
                 )
             }
         }
