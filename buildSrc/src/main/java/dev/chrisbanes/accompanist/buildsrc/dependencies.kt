cf175748e1a7a7eeb9ccb228d2a87f8aa95798cd
Chris Banes <chris@banes.me>
2020-11-11 18:03:49 +0000

    Update to Compose 1.0.0-alpha07

--- buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
@@ -57,8 +57,8 @@ object Libs {
         }
 
         object Compose {
-            const val snapshot = "6964664"
-            const val version = "1.0.0-SNAPSHOT"
+            const val snapshot = ""
+            const val version = "1.0.0-alpha07"
 
             @JvmStatic
             val snapshotUrl: String
