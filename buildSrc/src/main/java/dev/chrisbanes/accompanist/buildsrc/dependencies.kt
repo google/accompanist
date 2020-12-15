ba7650d36d8c8186bb97450d37e563b25ffd1d2f
Chris Banes <chris@banes.me>
2020-12-15 09:22:01 +0000

    Update to 7033025

--- buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
@@ -58,7 +58,7 @@ object Libs {
         }
 
         object Compose {
-            const val snapshot = "7030654"
+            const val snapshot = "7033025"
             const val version = "1.0.0-SNAPSHOT"
 
             @JvmStatic
