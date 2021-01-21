1ca2ee28a5a413795c31ba685ca36c6806aab452
Chris Banes <chris@banes.me>
2021-01-21 10:15:02 +0000

    Remove unused code

--- appcompat-theme/src/main/java/dev/chrisbanes/accompanist/appcompattheme/AppCompatTheme.kt
+++ appcompat-theme/src/main/java/dev/chrisbanes/accompanist/appcompattheme/AppCompatTheme.kt
@@ -18,7 +18,6 @@
 package dev.chrisbanes.accompanist.appcompattheme
 
 import android.content.Context
-import android.content.res.Resources
 import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
@@ -28,54 +27,31 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.AmbientContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.Density
 import androidx.core.content.res.use
-import java.lang.reflect.Method
 
 /**
- * A [MaterialTheme] which reads the corresponding values from a Material Components for Android
- * theme in the given [context].
- *
- * By default the text colors from any associated `TextAppearance`s from the theme are *not* read.
- * This is because setting a fixed color in the resulting [TextStyle] breaks the usage of
- * [androidx.compose.material.Emphasis] through [androidx.compose.material.ProvideEmphasis].
- * You can customize this through the [setTextColors] parameter.
+ * This function creates the components of a [androidx.compose.material.MaterialTheme], reading the
+ * values from the `Theme.AppCompat` Android theme.
  *
  * @param context The context to read the theme from.
  * @param readColors whether the read the MDC color palette from the context's theme.
  * If `false`, the current value of [MaterialTheme.colors] is preserved.
  * @param readTypography whether the read the MDC text appearances from [context]'s theme.
  * If `false`, the current value of [MaterialTheme.typography] is preserved.
- * @param readShapes whether the read the MDC shape appearances from the context's theme.
- * If `false`, the current value of [MaterialTheme.shapes] is preserved.
- * @param setTextColors whether to read the colors from the `TextAppearance`s associated from the
- * theme. Defaults to `false`.
+ * @param shapes A set of shapes to be used by the components in this hierarchy.
  */
 @Composable
 fun AppCompatTheme(
     context: Context = AmbientContext.current,
-    shapes: Shapes = MaterialTheme.shapes,
     readColors: Boolean = true,
     readTypography: Boolean = true,
-    setTextColors: Boolean = false,
+    shapes: Shapes = MaterialTheme.shapes,
     content: @Composable () -> Unit
 ) {
-    // We try and use the theme key value if available, which should be a perfect key for caching
-    // and avoid the expensive theme lookups in re-compositions.
-    //
-    // If the key is not available, we use the Theme itself as a rough approximation. Using the
-    // Theme instance as the key is not perfect, but it should work for 90% of cases.
-    // It falls down when the theme is manually mutated after a composition has happened
-    // (via `applyStyle()`, `rebase()`, `setTo()`), but the majority of apps do not use those.
-    val key = context.theme.key ?: context.theme
-
-    val themeParams = remember(key) {
-        createAppCompatTheme(
-            context = context,
+    val themeParams = remember(context.theme) {
+        context.createAppCompatTheme(
             readColors = readColors,
-            readTypography = readTypography,
-            setTextColors = setTextColors
+            readTypography = readTypography
         )
     }
 
@@ -98,201 +74,75 @@ data class ThemeParameters(
 
 /**
  * This function creates the components of a [androidx.compose.material.MaterialTheme], reading the
- * values from an Material Components for Android theme.
- *
- * By default the text colors from any associated `TextAppearance`s from the theme are *not* read.
- * This is because setting a fixed color in the resulting [TextStyle] breaks the usage of
- * [androidx.compose.material.Emphasis] through [androidx.compose.material.ProvideEmphasis].
- * You can customize this through the [setTextColors] parameter.
+ * values from the `Theme.AppCompat` Android theme.
  *
  * The individual components of the returned [ThemeParameters] may be `null`, depending on the
  * matching 'read' parameter. For example, if you set [readColors] to `false`,
  * [ThemeParameters.colors] will be null.
  *
- * @param context The context to read the theme from.
- * @param shapes TODO
- * @param density The current density.
  * @param readColors whether the read the MDC color palette from the context's theme.
  * @param readTypography whether the read the MDC text appearances from [context]'s theme.
- * @param readShapes whether the read the MDC shape appearances from the context's theme.
- * @param setTextColors whether to read the colors from the `TextAppearance`s associated from the
- * theme. Defaults to `false`.
- * @return [ThemeParameters] instance containing the resulting [Colors], [Typography]
- * and [Shapes].
- */
-@Suppress("UNUSED_PARAMETER")
-fun createAppCompatTheme(
-    context: Context,
-    density: Density = Density(context),
-    readColors: Boolean = true,
-    readTypography: Boolean = true,
-    setTextColors: Boolean = false
-): ThemeParameters {
-    return context.obtainStyledAttributes(R.styleable.AppCompatThemeAdapterTheme).use { ta ->
-        require(ta.hasValue(R.styleable.AppCompatThemeAdapterTheme_windowActionBar)) {
-            "createAppCompatTheme requires the host context's theme to extend Theme.AppCompat"
-        }
-
-        val colors: Colors? = if (readColors) {
-            val isLightTheme = ta.getBoolean(R.styleable.AppCompatThemeAdapterTheme_isLightTheme, true)
-
-            val defaultColors = if (isLightTheme) lightColors() else darkColors()
-
-            /* First we'll read the Material color palette */
-            val primary = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_colorPrimary)
-            val primaryVariant = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_colorPrimaryDark)
-            val onPrimary = primary.calculateOnColor()
-
-            val secondary = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_colorAccent)
-            // TODO: lighten/darken secondary for variant?
-            val secondaryVariant = secondary
-            val onSecondary = secondary.calculateOnColor()
-
-            val background = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_android_colorBackground)
-            val onBackground = background.calculateOnColor()
-
-            val error = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_colorError)
-            val onError = error.calculateOnColor()
-
-            defaultColors.copy(
-                primary = primary,
-                primaryVariant = primaryVariant,
-                onPrimary = onPrimary,
-                secondary = secondary,
-                secondaryVariant = secondaryVariant,
-                onSecondary = onSecondary,
-                background = background,
-                onBackground = onBackground,
-//                surface = surface,
-//                onSurface = onSurface,
-                error = error,
-                onError = onError
-            )
-        } else null
-
-        /**
-         * Next we'll create a typography instance, using the Material Theme text appearances
-         * for TextStyles.
-         *
-         * We create a normal 'empty' instance first to start from the defaults, then merge in our
-         * created text styles from the Android theme.
-         */
-
-        val typography = if (readTypography) {
-            Typography().merge(
-//                h1 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceHeadline1),
-//                    setTextColors
-//                ),
-//                h2 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceHeadline2),
-//                    setTextColors
-//                ),
-//                h3 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceHeadline3),
-//                    setTextColors
-//                ),
-//                h4 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceHeadline4),
-//                    setTextColors
-//                ),
-//                h5 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceHeadline5),
-//                    setTextColors
-//                ),
-//                h6 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceHeadline6),
-//                    setTextColors
-//                ),
-//                subtitle1 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceSubtitle1),
-//                    setTextColors
-//                ),
-//                subtitle2 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceSubtitle2),
-//                    setTextColors
-//                ),
-//                body1 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceBody1),
-//                    setTextColors
-//                ),
-//                body2 = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceBody2),
-//                    setTextColors
-//                ),
-//                button = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceButton),
-//                    setTextColors
-//                ),
-//                caption = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceCaption),
-//                    setTextColors
-//                ),
-//                overline = textStyleFromTextAppearance(
-//                    context,
-//                    density,
-//                    ta.getResourceIdOrThrow(R.styleable.AppCompatThemeAdapterTheme_textAppearanceOverline),
-//                    setTextColors
-//                )
-            )
-        } else null
-
-        ThemeParameters(colors, typography)
-    }
-}
-
-/**
- * This is gross, but we need a way to check for theme equality. Theme does not implement
- * `equals()` or `hashCode()`, but it does have a hidden method called `getKey()`.
  *
- * The cost of this reflective invoke is a lot cheaper than the full theme read which can
- * happen on each re-composition.
+ * @return [ThemeParameters] instance containing the resulting [Colors] and [Typography]
  */
-private inline val Resources.Theme.key: Any?
-    get() {
-        if (!sThemeGetKeyMethodFetched) {
-            try {
-                @Suppress("PrivateApi")
-                sThemeGetKeyMethod = Resources.Theme::class.java.getDeclaredMethod("getKey")
-                    .apply { isAccessible = true }
-            } catch (e: ReflectiveOperationException) {
-                // Failed to retrieve Theme.getKey method
-            }
-            sThemeGetKeyMethodFetched = true
-        }
-        if (sThemeGetKeyMethod != null) {
-            return try {
-                sThemeGetKeyMethod?.invoke(this)
-            } catch (e: ReflectiveOperationException) {
-                // Failed to invoke Theme.getKey()
-            }
-        }
-        return null
+fun Context.createAppCompatTheme(
+    readColors: Boolean = true,
+    readTypography: Boolean = true
+): ThemeParameters = obtainStyledAttributes(R.styleable.AppCompatThemeAdapterTheme).use { ta ->
+    require(ta.hasValue(R.styleable.AppCompatThemeAdapterTheme_windowActionBar)) {
+        "createAppCompatTheme requires the host context's theme to extend Theme.AppCompat"
     }
 
-private var sThemeGetKeyMethodFetched = false
-private var sThemeGetKeyMethod: Method? = null
+    val colors = if (readColors) {
+        val isLightTheme = ta.getBoolean(R.styleable.AppCompatThemeAdapterTheme_isLightTheme, true)
+
+        val defaultColors = if (isLightTheme) lightColors() else darkColors()
+
+        /* First we'll read the Material color palette */
+        val primary = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_colorPrimary)
+        val primaryVariant = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_colorPrimaryDark)
+        val onPrimary = primary.calculateOnColor()
+
+        val secondary = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_colorAccent)
+        // TODO: lighten/darken secondary for variant?
+        val secondaryVariant = secondary
+        val onSecondary = secondary.calculateOnColor()
+
+        val background = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_android_colorBackground)
+        val onBackground = background.calculateOnColor()
+
+        val error = ta.getComposeColor(R.styleable.AppCompatThemeAdapterTheme_colorError)
+        val onError = error.calculateOnColor()
+
+        defaultColors.copy(
+            primary = primary,
+            primaryVariant = primaryVariant,
+            onPrimary = onPrimary,
+            secondary = secondary,
+            secondaryVariant = secondaryVariant,
+            onSecondary = onSecondary,
+            background = background,
+            onBackground = onBackground,
+            error = error,
+            onError = onError
+        )
+    } else null
+
+    /**
+     * Next we'll create a typography instance. We only use the default app:fontFamily or
+     * android:fontFamily set in the theme. If neither of these are set, we return null.
+     */
+    /**
+     * Next we'll create a typography instance. We only use the default app:fontFamily or
+     * android:fontFamily set in the theme. If neither of these are set, we return null.
+     */
+    val typography = if (readTypography) {
+        val fontFamily = ta.getFontFamilyOrNull(R.styleable.AppCompatThemeAdapterTheme_fontFamily)
+            ?: ta.getFontFamilyOrNull(R.styleable.AppCompatThemeAdapterTheme_android_fontFamily)
+        fontFamily?.let {
+            Typography(defaultFontFamily = it.fontFamily)
+        }
+    } else null
+
+    ThemeParameters(colors, typography)
+}
--- appcompat-theme/src/main/java/dev/chrisbanes/accompanist/appcompattheme/TypedArrayUtils.kt
+++ appcompat-theme/src/main/java/dev/chrisbanes/accompanist/appcompattheme/TypedArrayUtils.kt
@@ -16,108 +16,15 @@
 
 package dev.chrisbanes.accompanist.appcompattheme
 
-import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.Typeface
-import android.os.Build
 import android.util.TypedValue
-import androidx.annotation.StyleRes
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.asFontFamily
 import androidx.compose.ui.text.font.font
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.em
-import androidx.compose.ui.unit.sp
 import androidx.core.content.res.getColorOrThrow
-import androidx.core.content.res.use
 import kotlin.concurrent.getOrSet
-
-internal fun textStyleFromTextAppearance(
-    context: Context,
-    density: Density,
-    @StyleRes id: Int,
-    setTextColors: Boolean
-): TextStyle {
-    return context.obtainStyledAttributes(id, R.styleable.AppCompatThemeAdapterTextAppearance).use { a ->
-        val textStyle = a.getInt(R.styleable.AppCompatThemeAdapterTextAppearance_android_textStyle, -1)
-        val textFontWeight = a.getInt(R.styleable.AppCompatThemeAdapterTextAppearance_android_textFontWeight, -1)
-        val typeface = a.getInt(R.styleable.AppCompatThemeAdapterTextAppearance_android_typeface, -1)
-
-        // TODO read and expand android:fontVariationSettings.
-        // Variable fonts are not supported in Compose yet
-
-        // FYI, this only works with static font files in assets
-        val fontFamily: FontFamilyWithWeight? = a.getFontFamilyOrNull(
-            R.styleable.AppCompatThemeAdapterTextAppearance_fontFamily
-        ) ?: a.getFontFamilyOrNull(R.styleable.AppCompatThemeAdapterTextAppearance_android_fontFamily)
-
-        TextStyle(
-            color = when {
-                setTextColors -> {
-                    a.getComposeColor(R.styleable.AppCompatThemeAdapterTextAppearance_android_textColor)
-                }
-                else -> Color.Unspecified
-            },
-            fontSize = a.getTextUnit(R.styleable.AppCompatThemeAdapterTextAppearance_android_textSize, density),
-            lineHeight = run {
-                a.getTextUnitOrNull(R.styleable.AppCompatThemeAdapterTextAppearance_lineHeight, density)
-                    ?: a.getTextUnitOrNull(R.styleable.AppCompatThemeAdapterTextAppearance_android_lineHeight, density)
-                    ?: TextUnit.Unspecified
-            },
-            fontFamily = when {
-                fontFamily != null -> fontFamily.fontFamily
-                // Values below are from frameworks/base attrs.xml
-                typeface == 1 -> FontFamily.SansSerif
-                typeface == 2 -> FontFamily.Serif
-                typeface == 3 -> FontFamily.Monospace
-                else -> null
-            },
-            fontStyle = when {
-                (textStyle and Typeface.ITALIC) != 0 -> FontStyle.Italic
-                else -> FontStyle.Normal
-            },
-            fontWeight = when {
-                textFontWeight in 0..149 -> FontWeight.W100
-                textFontWeight in 150..249 -> FontWeight.W200
-                textFontWeight in 250..349 -> FontWeight.W300
-                textFontWeight in 350..449 -> FontWeight.W400
-                textFontWeight in 450..549 -> FontWeight.W500
-                textFontWeight in 550..649 -> FontWeight.W600
-                textFontWeight in 650..749 -> FontWeight.W700
-                textFontWeight in 750..849 -> FontWeight.W800
-                textFontWeight in 850..999 -> FontWeight.W900
-                // Else, check the text style for bold
-                (textStyle and Typeface.BOLD) != 0 -> FontWeight.Bold
-                // Else, the font family might have an implicit weight (san-serif-light, etc)
-                fontFamily != null -> fontFamily.weight
-                else -> null
-            },
-            fontFeatureSettings = a.getString(R.styleable.AppCompatThemeAdapterTextAppearance_android_fontFeatureSettings),
-            shadow = run {
-                val shadowColor = a.getComposeColor(R.styleable.AppCompatThemeAdapterTextAppearance_android_shadowColor)
-                if (shadowColor != Color.Unspecified) {
-                    val dx = a.getFloat(R.styleable.AppCompatThemeAdapterTextAppearance_android_shadowDx, 0f)
-                    val dy = a.getFloat(R.styleable.AppCompatThemeAdapterTextAppearance_android_shadowDy, 0f)
-                    val rad = a.getFloat(R.styleable.AppCompatThemeAdapterTextAppearance_android_shadowRadius, 0f)
-                    Shadow(color = shadowColor, offset = Offset(dx, dy), blurRadius = rad)
-                } else null
-            },
-            letterSpacing = when {
-                a.hasValue(R.styleable.AppCompatThemeAdapterTextAppearance_android_letterSpacing) -> {
-                    a.getFloat(R.styleable.AppCompatThemeAdapterTextAppearance_android_letterSpacing, 0f).em
-                }
-                else -> TextUnit.Unspecified
-            }
-        )
-    }
-}
 
 private val tempTypedValue = ThreadLocal<TypedValue>()
 
@@ -128,24 +35,12 @@ internal fun TypedArray.getComposeColor(
 
 /**
  * Returns the given index as a [FontFamily] and [FontWeight],
- * or [fallback] if the value can not be coerced to a [FontFamily].
- *
- * @param index index of attribute to retrieve.
- * @param fallback Value to return if the attribute is not defined or cannot be coerced to a
- * [FontFamily].
- */
-internal fun TypedArray.getFontFamily(index: Int, fallback: FontFamily): FontFamilyWithWeight {
-    return getFontFamilyOrNull(index) ?: FontFamilyWithWeight(fallback)
-}
-
-/**
- * Returns the given index as a [FontFamily] and [FontWeight],
  * or `null` if the value can not be coerced to a [FontFamily].
  *
  * @param index index of attribute to retrieve.
  */
 internal fun TypedArray.getFontFamilyOrNull(index: Int): FontFamilyWithWeight? {
-    val tv = tempTypedValue.getOrSet { TypedValue() }
+    val tv = tempTypedValue.getOrSet(::TypedValue)
     if (getValue(index, tv) && tv.type == TypedValue.TYPE_STRING) {
         return when (tv.string) {
             "sans-serif" -> FontFamilyWithWeight(FontFamily.SansSerif)
@@ -174,53 +69,3 @@ internal data class FontFamilyWithWeight(
     val fontFamily: FontFamily,
     val weight: FontWeight = FontWeight.Normal
 )
-
-/**
- * Returns the given index as a [TextUnit], or [fallback] if the value can not be coerced to
- * a [TextUnit].
- *
- * @param index index of attribute to retrieve.
- * @param density the current display density.
- * @param fallback Value to return if the attribute is not defined or cannot be coerced to
- * a [TextUnit].
- */
-internal fun TypedArray.getTextUnit(
-    index: Int,
-    density: Density,
-    fallback: TextUnit = TextUnit.Unspecified
-): TextUnit = getTextUnitOrNull(index, density) ?: fallback
-
-/**
- * Returns the given index as a [TextUnit], or `null` if the value can not be coerced to
- * a [TextUnit].
- *
- * @param index index of attribute to retrieve.
- * @param density the current display density.
- */
-internal fun TypedArray.getTextUnitOrNull(
-    index: Int,
-    density: Density
-): TextUnit? {
-    val tv = tempTypedValue.getOrSet { TypedValue() }
-    if (getValue(index, tv) && tv.type == TypedValue.TYPE_DIMENSION) {
-        return when (tv.complexUnitCompat) {
-            // For SP values, we convert the value directly to an TextUnit.Sp
-            TypedValue.COMPLEX_UNIT_SP -> TypedValue.complexToFloat(tv.data).sp
-            // For DIP values, we convert the value to an TextUnit.Em (roughly equivalent)
-            TypedValue.COMPLEX_UNIT_DIP -> TypedValue.complexToFloat(tv.data).em
-            // For another other types, we let the TypedArray flatten to a px value, and
-            // we convert it to an Sp based on the current density
-            else -> with(density) { getDimension(index, 0f).toSp() }
-        }
-    }
-    return null
-}
-
-/**
- * A workaround since [TypedValue.getComplexUnit] is API 22+
- */
-private inline val TypedValue.complexUnitCompat
-    get() = when {
-        Build.VERSION.SDK_INT > 22 -> complexUnit
-        else -> TypedValue.COMPLEX_UNIT_MASK and (data shr TypedValue.COMPLEX_UNIT_SHIFT)
-    }
