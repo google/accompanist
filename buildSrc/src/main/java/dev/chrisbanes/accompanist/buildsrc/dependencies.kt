036fcec6caa2204f5bd00b1ba365de36ae650c59
Chris Banes <chris@banes.me>
2021-02-10 18:00:39 +0000

    Update to Compose 1.0.0-alpha12

--- buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
@@ -62,8 +62,8 @@ object Libs {
         const val appcompat = "androidx.appcompat:appcompat:1.2.0"
 
         object Compose {
-            const val snapshot = "7123292"
-            const val version = "1.0.0-SNAPSHOT"
+            const val snapshot = ""
+            const val version = "1.0.0-alpha12"
 
             @JvmStatic
             val snapshotUrl: String
@@ -97,7 +97,7 @@ object Libs {
         const val coreAlpha = "androidx.core:core:1.5.0-beta01"
 
         object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.3.0-SNAPSHOT"
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha01"
         }
     }
 
