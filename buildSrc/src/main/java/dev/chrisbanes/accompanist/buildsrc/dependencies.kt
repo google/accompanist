ff5561a2d2e51a01cf6ee8b17e9d209e6e10d23f
Chris Banes <chris@banes.me>
2021-02-11 08:04:52 +0000

    Update to activity-compose alpha02

--- buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/dev/chrisbanes/accompanist/buildsrc/dependencies.kt
@@ -97,7 +97,7 @@ object Libs {
         const val coreAlpha = "androidx.core:core:1.5.0-beta01"
 
         object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha01"
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha02"
         }
     }
 
