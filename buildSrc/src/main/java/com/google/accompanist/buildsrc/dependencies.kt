6b6bf2bb2421139c6853ebe3e2b383c23edf5703
Chris Banes <chris@banes.me>
2021-03-16 22:25:38 +0000

    Revert "Merge pull request #246 from google/cb/dokka-1.4.30"
    e5521e47e0369931b8525fdd9f3fe0ccc5de48ce

--- buildSrc/src/main/java/com/google/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/google/accompanist/buildsrc/dependencies.kt
@@ -36,7 +36,7 @@ object Libs {
     }
 
     object Dokka {
-        private const val version = "1.4.30"
+        private const val version = "1.4.20"
         const val gradlePlugin = "org.jetbrains.dokka:dokka-gradle-plugin:$version"
     }
 
