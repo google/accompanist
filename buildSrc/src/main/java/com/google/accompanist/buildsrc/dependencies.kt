c7e2ec8987f90b8c0fe450e7ca57f1e27364a763
Chris Banes <chris@banes.me>
2021-04-05 17:50:58 +0100

    Update to Coil 1.1.1

--- buildSrc/src/main/java/com/google/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/google/accompanist/buildsrc/dependencies.kt
@@ -104,7 +104,7 @@ object Libs {
     }
 
     object Coil {
-        private const val version = "1.0.0"
+        private const val version = "1.1.1"
         const val coil = "io.coil-kt:coil:$version"
         const val gif = "io.coil-kt:coil-gif:$version"
     }
