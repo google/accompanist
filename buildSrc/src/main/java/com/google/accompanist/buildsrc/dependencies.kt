0c26b330b365a225d31f17ac1aed34cd2a2b7838
Nick Butcher <nickbutcher@google.com>
2021-04-07 15:30:37 +0100

    Update activity-compose to snapshot.

--- buildSrc/src/main/java/com/google/accompanist/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/google/accompanist/buildsrc/dependencies.kt
@@ -99,7 +99,7 @@ object Libs {
         const val core1_5_0 = "androidx.core:core:1.5.0-beta03"
 
         object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha05"
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0-SNAPSHOT"
         }
     }
 
