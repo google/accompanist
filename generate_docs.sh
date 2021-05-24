289289f7b33491877df2f4543e3458fba03196c9
Chris Banes <chris@banes.me>
2021-05-24 12:00:21 +0100

    Update dokka to 1.4.32
    Also removed an unnecessary string replace

--- generate_docs.sh
+++ generate_docs.sh
@@ -31,11 +31,6 @@ sed -i.bak 's/$dokka.linkExtension:md/$dokka.linkExtension:html/g' package-list-
 # Build the docs with dokka
 ./gradlew dokkaHtmlMultiModule
 
-# Re-word the Dokka call out
-find docs/api/ -type f -name '*.html' -exec sed -i -e 's/Sponsored and developed/Documentation generated/g' {} \;
-# Remove the copyright declaration
-find docs/api/ -type f -name '*.html' -exec sed -i -e 's/© [0-9]* Copyright//' {} \;
-
 # Clean up the temp Coil package list
 rm package-list-coil-base
 
