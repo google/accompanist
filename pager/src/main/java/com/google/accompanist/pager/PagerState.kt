4e24003060bbd65f90d2a86641c800d76a35bde2
Halil Ozercan <halilozercan@gmail.com>
2021-06-21 13:32:34 +0300

    Fix floorMod operation in PagerState to account for high values like Int.MAX_VALUE
    - New implementation uses the actual floorMod algorithm from JDK 1.8 by using floorDiv

--- pager/src/main/java/com/google/accompanist/pager/PagerState.kt
+++ pager/src/main/java/com/google/accompanist/pager/PagerState.kt
@@ -673,7 +673,7 @@ class PagerState(
         private fun Int.floorMod(other: Int): Int {
             return when (other) {
                 0 -> this
-                else -> (this % other + other) % other
+                else -> this - this.floorDiv(other) * other
             }
         }
     }
