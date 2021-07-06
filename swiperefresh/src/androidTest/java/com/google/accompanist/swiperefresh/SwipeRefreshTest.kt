92e4d6276058d39ead9ca26e71aa614fecdf2524
Sita Bérété <sita.berete.3@gmail.com>
2021-07-06 17:34:03 -0400

    [SwipeRefresh] test bottom swipe to refresh

--- swiperefresh/src/androidTest/java/com/google/accompanist/swiperefresh/SwipeRefreshTest.kt
+++ swiperefresh/src/androidTest/java/com/google/accompanist/swiperefresh/SwipeRefreshTest.kt
@@ -26,6 +26,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
@@ -33,7 +34,9 @@ import androidx.compose.ui.test.getUnclippedBoundsInRoot
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performGesture
+import androidx.compose.ui.test.performScrollToIndex
 import androidx.compose.ui.test.swipeDown
+import androidx.compose.ui.test.swipeUp
 import com.google.common.truth.Truth.assertThat
 import org.junit.Ignore
 import org.junit.Rule
@@ -42,6 +45,18 @@ import org.junit.Test
 class SwipeRefreshTest {
     @get:Rule
     val rule = createComposeRule()
+
+    private val swipeRefreshNode: SemanticsNodeInteraction
+        get() = rule.onNodeWithTag(SwipeRefreshTag)
+
+    private val indicatorNode: SemanticsNodeInteraction
+        get() = rule.onNodeWithTag(SwipeRefreshIndicatorTag)
+
+    private val bottomIndicatorNode: SemanticsNodeInteraction
+        get() = rule.onNodeWithTag(BottomSwipeRefreshIndicatorTag)
+
+    private val listNode: SemanticsNodeInteraction
+        get() = rule.onNodeWithTag(ListTag)
 
     @Test
     @Ignore("https://issuetracker.google.com/issues/185814751")
@@ -119,33 +134,104 @@ class SwipeRefreshTest {
         assertThat(indicatorNode.getUnclippedBoundsInRoot()).isEqualTo(restingBounds)
     }
 
-    private val swipeRefreshNode: SemanticsNodeInteraction
-        get() = rule.onNodeWithTag(SwipeRefreshTag)
+    // ------------------------- BOTTOM SWIPE TO REFRESH TESTS --------------------------- //
 
-    private val indicatorNode: SemanticsNodeInteraction
-        get() = rule.onNodeWithTag(SwipeRefreshIndicatorTag)
+    @Test
+    @OptIn(ExperimentalTestApi::class)
+    fun bottomSwipeRefreshes_shouldRefresh() {
+        val state = SwipeRefreshState(false)
+        var refreshCallCount = 0
+
+        rule.setContent {
+            SwipeRefreshTestContent(bottomSwipeRefreshState = state) {
+                state.isRefreshing = true
+                refreshCallCount++
+            }
+        }
+
+        // Scroll to the end of the list
+        listNode.performScrollToIndex(ListItemCount - 1)
+
+        // Swipe up on the swipe refresh
+        swipeRefreshNode.performGesture { swipeUp() }
+
+        // Assert that the onRefresh lambda was called and that we're refreshing
+        assertThat(refreshCallCount).isEqualTo(1)
+        assertThat(state.isRefreshing).isTrue()
+
+        // Assert that the indicator is displayed
+        bottomIndicatorNode.assertIsDisplayed()
+
+        // TODO: Test if the indicator is fading out after we set [state.isRefreshing] to false,
+        //  once https://issuetracker.google.com/issues/185814751 is solved.
+    }
+
+    @Test
+    fun refreshingBottomIndicator_returnsToRest() {
+        rule.setContent {
+            SwipeRefreshTestContent(bottomSwipeRefreshState = SwipeRefreshState(true)) {}
+        }
+
+        // Assert that the indicator is displayed
+        bottomIndicatorNode.assertIsDisplayed()
+        // Store the 'resting' bounds
+        val restingBounds = bottomIndicatorNode.getUnclippedBoundsInRoot()
+
+        // Now swipe up. The indicator should react visually
+        swipeRefreshNode.performGesture { swipeUp() }
+
+        // Assert that the indicator returns back to it's 'resting' position
+        assertThat(bottomIndicatorNode.getUnclippedBoundsInRoot()).isEqualTo(restingBounds)
+    }
+
+    @Test
+    fun bottomIndicator_refreshingInitially() {
+        rule.setContent {
+            SwipeRefreshTestContent(bottomSwipeRefreshState = rememberSwipeRefreshState(true)) {}
+        }
+
+        // Assert that the indicator is displayed
+        bottomIndicatorNode.assertIsDisplayed()
+    }
+
 }
 
 private const val SwipeRefreshTag = "swipe_refresh"
 private const val SwipeRefreshIndicatorTag = "swipe_refresh_indicator"
+private const val BottomSwipeRefreshIndicatorTag = "bottom_swipe_refresh_indicator"
+private const val ListTag = "list_tag"
+private const val ListItemCount = 30
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun SwipeRefreshTestContent(
-    state: SwipeRefreshState,
-    onRefresh: () -> Unit,
+    topSwipeRefreshState: SwipeRefreshState? = null,
+    bottomSwipeRefreshState: SwipeRefreshState? = null,
+    onRefresh: (SwipeRefreshPosition) -> Unit,
 ) {
     MaterialTheme {
         SwipeRefresh(
-            topSwipeRefreshState = state,
+            topSwipeRefreshState = topSwipeRefreshState,
+            bottomSwipeRefreshState = bottomSwipeRefreshState,
             onRefresh = onRefresh,
             modifier = Modifier.testTag(SwipeRefreshTag),
-            indicator = { state, trigger ->
+            topIndicator = { state, trigger ->
                 SwipeRefreshIndicator(state, trigger, Modifier.testTag(SwipeRefreshIndicatorTag))
-            }
+            },
+            bottomIndicator = { state, trigger ->
+                SwipeRefreshIndicator(
+                    state,
+                    trigger,
+                    Modifier.testTag(BottomSwipeRefreshIndicatorTag)
+                )
+            },
         ) {
-            LazyColumn(Modifier.fillMaxSize()) {
-                items(30) { index ->
+            LazyColumn(
+                Modifier
+                    .fillMaxSize()
+                    .testTag(ListTag)
+            ) {
+                items(ListItemCount) { index ->
                     ListItem(Modifier.fillMaxWidth()) {
                         Text(text = "Item $index")
                     }
