be2ab74c28ad402daa734d29d3dd6f3fbee84346
Sita Bérété <sita.berete.3@gmail.com>
2021-07-06 18:27:13 -0400

    [SwipeRefresh] remove unused classes and functions

--- swiperefresh/src/main/java/com/google/accompanist/swiperefresh/Slingshot.kt
+++ swiperefresh/src/main/java/com/google/accompanist/swiperefresh/Slingshot.kt
@@ -76,46 +76,6 @@ internal fun rememberUpdatedSlingshot(
     }
 }
 
-@Composable
-internal fun rememberUpdatedBottomSlingshot(
-    offsetY: Float,
-    maxOffsetY: Float,
-    height: Int
-): Slingshot {
-    val offsetPercent = min(1f, offsetY / maxOffsetY)
-    val adjustedPercent = max(offsetPercent - 0.4f, 0f) * 5 / 3
-    val extraOffset = abs(offsetY) - maxOffsetY
-
-    // Can accommodate custom start and slingshot distance here
-    val slingshotDistance = maxOffsetY
-    val tensionSlingshotPercent = max(
-        0f, min(extraOffset, slingshotDistance * 2) / slingshotDistance
-    )
-    val tensionPercent = (
-        (tensionSlingshotPercent / 4) -
-            (tensionSlingshotPercent / 4).pow(2)
-        ) * 2
-    val extraMove = slingshotDistance * tensionPercent * 2
-    val targetY = height + ((slingshotDistance * offsetPercent) + extraMove).toInt()
-    val offset = targetY //- height
-    val strokeStart = adjustedPercent * 0.8f
-
-    val startTrim = 0f
-    val endTrim = strokeStart.coerceAtMost(MaxProgressArc)
-
-    val rotation = (-0.25f + 0.4f * adjustedPercent + tensionPercent * 2) * 0.5f
-    val arrowScale = min(1f, adjustedPercent)
-
-    return remember { Slingshot() }.apply {
-        this.offset = offset
-        this.startTrim = startTrim
-        this.endTrim = endTrim
-        this.rotation = rotation
-        this.arrowScale = arrowScale
-    }
-}
-
-
 @Stable
 internal class Slingshot {
     var offset: Int by mutableStateOf(0)
