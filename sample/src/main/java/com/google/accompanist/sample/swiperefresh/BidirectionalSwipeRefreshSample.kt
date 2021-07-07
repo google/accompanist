4f3ec877c0969d1b6c5259b0d333e2d2ddabd076
Sita Bérété <sita.berete.3@gmail.com>
2021-07-07 13:42:42 -0400

    [SwipeRefresh] add bidirectional swipe to refresh sample

---
+++ sample/src/main/java/com/google/accompanist/sample/swiperefresh/BidirectionalSwipeRefreshSample.kt
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.accompanist.sample.swiperefresh
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.glide.rememberGlidePainter
+import com.google.accompanist.sample.AccompanistSampleTheme
+import com.google.accompanist.sample.R
+import com.google.accompanist.sample.randomSampleImageUrl
+import com.google.accompanist.swiperefresh.RefreshIndicatorPosition
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import kotlinx.coroutines.delay
+
+class BidirectionalSwipeRefreshSample : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AccompanistSampleTheme {
+                Sample()
+            }
+        }
+    }
+}
+
+@Composable
+private fun Sample() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.swiperefresh_title_bidirectional)) },
+                backgroundColor = MaterialTheme.colors.surface,
+            )
+        },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var isBottomIndicatorRefreshing by remember { mutableStateOf(false) }
+        var isTopIndicatorRefreshing by remember { mutableStateOf(false) }
+
+        // Simulate a fake 2-second 'load'. Ideally this 'refreshing' value would
+        // come from a ViewModel or similar
+        LaunchedEffect(isBottomIndicatorRefreshing) {
+            if (isBottomIndicatorRefreshing) {
+                delay(2000)
+                isBottomIndicatorRefreshing = false
+            }
+        }
+
+        // Simulate a fake 2-second 'load' as the LaunchedEffect above
+        LaunchedEffect(isTopIndicatorRefreshing) {
+            if (isTopIndicatorRefreshing) {
+                delay(2000)
+                isTopIndicatorRefreshing = false
+            }
+        }
+
+        SwipeRefresh(
+            topRefreshIndicatorState = rememberSwipeRefreshState(isRefreshing = isTopIndicatorRefreshing),
+            bottomRefreshIndicatorState = rememberSwipeRefreshState(isRefreshing = isBottomIndicatorRefreshing),
+            onRefresh = { refreshIndicatorPosition ->
+                when (refreshIndicatorPosition) {
+                    RefreshIndicatorPosition.TOP -> isTopIndicatorRefreshing = true
+                    RefreshIndicatorPosition.BOTTOM -> isBottomIndicatorRefreshing = true
+                }
+            },
+        ) {
+            LazyColumn {
+                items(30) { index ->
+                    Row(Modifier.padding(16.dp)) {
+                        Image(
+                            painter = rememberGlidePainter(randomSampleImageUrl(index)),
+                            contentDescription = null,
+                            modifier = Modifier.size(64.dp),
+                        )
+
+                        Spacer(Modifier.width(8.dp))
+
+                        Text(
+                            text = "Text",
+                            style = MaterialTheme.typography.subtitle2,
+                            modifier = Modifier
+                                .weight(1f)
+                                .align(Alignment.CenterVertically)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
