2a5154848376a23011df9c7aab24f2eb41507784
Nick Butcher <nickbutcher@google.com>
2021-10-04 12:59:35 +0100

    Fix tests for performGesture changes.

--- pager/src/sharedTest/kotlin/com/google/accompanist/pager/HorizontalPagerScrollingContentTest.kt
+++ pager/src/sharedTest/kotlin/com/google/accompanist/pager/HorizontalPagerScrollingContentTest.kt
@@ -81,7 +81,7 @@ class HorizontalPagerScrollingContentTest {
         // Perform a very quick, high velocity scroll which will scroll the inner content to it's
         // opposite/end edge
         rule.onNodeWithTag(TestTag)
-            .swipeAcrossCenterWithVelocity(velocityPerSec = 5_000.dp, distancePercentageX = -0.5f)
+            .swipeAcrossCenterWithVelocity(velocityPerSec = 2_000.dp, distancePercentageX = -0.5f)
 
         // Wait for the flings to end
         rule.waitForIdle()
