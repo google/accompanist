c41e2d99aa0709eb5d5869880bad08e9e282ad23
Lorenzo Neumann <36760115+ln-12@users.noreply.github.com>
2022-02-08 09:56:23 +0100

    Add LayoutParams.MATCH_PARENT in webview

--- web/src/main/java/com/google/accompanist/web/WebView.kt
+++ web/src/main/java/com/google/accompanist/web/WebView.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import android.view.ViewGroup
 
 /**
  * A wrapper around the Android View WebView to provide a basic WebView composable.
@@ -71,6 +72,11 @@ fun WebView(
         factory = { context ->
             WebView(context).apply {
                 onCreated(this)
+
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
 
                 webViewClient = object : WebViewClient() {
                     override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
