de5b44bbe1cfd91e32ea33276173ba77361c193c
Ben Trengrove <bentrengrove@google.com>
2022-11-03 13:57:20 +1100

    Migrate placeholder sample

--- sample/src/main/java/com/google/accompanist/sample/swiperefresh/SwipeRefreshBasicSample.kt
+++ sample/src/main/java/com/google/accompanist/sample/swiperefresh/SwipeRefreshBasicSample.kt
@@ -20,7 +20,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -63,7 +62,7 @@ class SwipeRefreshBasicSample : ComponentActivity() {
     }
 }
 
-//@Suppress("DEPRECATION")
+// @Suppress("DEPRECATION")
 @Suppress("DEPRECATION")
 @OptIn(ExperimentalCoilApi::class)
 @Composable
