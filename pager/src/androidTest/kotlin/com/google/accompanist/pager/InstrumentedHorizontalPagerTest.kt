0a0ec524b14eba7a46c1185d8484beab4466c4e2
Rebecca Franks <riggaroo@google.com>
2023-02-06 13:31:04 +0000

    spotless

--- pager/src/androidTest/kotlin/com/google/accompanist/pager/InstrumentedHorizontalPagerTest.kt
+++ pager/src/androidTest/kotlin/com/google/accompanist/pager/InstrumentedHorizontalPagerTest.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.google.accompanist.pager
 
 import androidx.compose.foundation.layout.PaddingValues
