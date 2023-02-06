3044dc03da8a15e0bddc51ae1037044902445faa
Rebecca Franks <riggaroo@google.com>
2023-02-06 12:46:27 +0000

    Ignore deprecation in tests

--- pager/src/sharedTest/kotlin/com/google/accompanist/pager/BaseHorizontalPagerTest.kt
+++ pager/src/sharedTest/kotlin/com/google/accompanist/pager/BaseHorizontalPagerTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+@file:Suppress("DEPRECATION")
 package com.google.accompanist.pager
 
 import androidx.compose.foundation.background
--- pager/src/sharedTest/kotlin/com/google/accompanist/pager/TestUtils.kt
+++ pager/src/sharedTest/kotlin/com/google/accompanist/pager/TestUtils.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.google.accompanist.pager
 
 import androidx.compose.ui.graphics.Color
