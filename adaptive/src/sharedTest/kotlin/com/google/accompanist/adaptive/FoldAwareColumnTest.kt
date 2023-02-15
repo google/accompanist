77fc457e705de83cd2043c86bce8f356753f0fc5
Kristen Halper <khalper@microsoft.com>
2023-02-15 10:43:00 -0800

    Fix lint errors

--- adaptive/src/sharedTest/kotlin/com/google/accompanist/adaptive/FoldAwareColumnTest.kt
+++ adaptive/src/sharedTest/kotlin/com/google/accompanist/adaptive/FoldAwareColumnTest.kt
@@ -53,7 +53,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
 @RunWith(AndroidJUnit4::class)
 class FoldAwareColumnTest {
     private val testTag = "FoldAwareColumnTestTag"
