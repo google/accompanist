5670636e1eee3cda8c5a3f0bed47f46ca9a091ec
Odin Asbjørnsen <odinas004@gmail.com>
2023-05-18 17:42:10 +0200

    Migrate permissions-lint to Kotlin DSL

--- permissions-lint/build.gradle.kts
+++ permissions-lint/build.gradle.kts
@@ -15,17 +15,17 @@
  */
 
 plugins {
-    id("java-library")
+    `java-library`
     id("kotlin")
-    id("org.jetbrains.dokka")
-    id("com.android.lint")
+    id(libs.plugins.jetbrains.dokka.get().pluginId)
+    id(libs.plugins.android.lint.get().pluginId)
 }
 
 kotlin {
     explicitApi()
 }
 
-lintOptions {
+lint {
     htmlReport = true
     htmlOutput = file("lint-report.html")
     textReport = true
@@ -43,26 +43,26 @@ affectedTestConfiguration {
  * not currently support dependencies, so instead we need to bundle any dependencies with the
  * lint jar manually. (b/182319899)
  */
-def bundle = configurations.create("bundleInside")
+val bundleInside: Configuration = configurations.create("bundleInside")
 // bundleInside dependencies should be included as compileOnly and testImplementation as well
-configurations.getByName("compileOnly").setExtendsFrom([bundle])
-configurations.getByName("testImplementation").setExtendsFrom([bundle])
-tasks.named("jar").configure { task ->
-    def jarTask = task as Jar
-    jarTask.dependsOn(bundle)
-    jarTask.from({
-        bundle
-                // The stdlib is already bundled with lint, so no need to include it manually
-                // in the lint.jar if any dependencies here depend on it
-                .filter { !it.name.contains("kotlin-stdlib") }
-                .collect { file ->
-                    if (file.isDirectory()) {
-                        file
-                    } else {
-                        zipTree(file)
-                    }
-                }
-    })
+configurations.getByName("compileOnly").setExtendsFrom(setOf(bundleInside))
+configurations.getByName("testImplementation").setExtendsFrom(setOf(bundleInside))
+
+tasks.getByName<Jar>("jar") {
+    this.dependsOn(bundleInside)
+    this.from({
+       bundleInside
+               // The stdlib is already bundled with lint, so no need to include it manually
+               // in the lint.jar if any dependencies here depend on it
+               .filter { !it.name.contains("kotlin-stdlib") }
+               .map { file ->
+                   if (file.isDirectory) {
+                       file
+                   } else {
+                       zipTree(file)
+                   }
+               }
+   })
 }
 
 dependencies {
